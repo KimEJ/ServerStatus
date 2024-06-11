1bf629e5a5d49acb5833e3df70fea4fc992988cb
ujkim <ujkim@interpass.kr>
2024-06-11 10:26:02 +0900

    fe 수정중

---
+++ fe/src/components/ServerList.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container class="fill-height">
+    <v-data-iterator :items="[1, 2, 3, 4, 5]">
+      <template v-slot:default="{ items }">
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :title="`Item ${i}`"
+        ></v-list-item>
+      </template>
+    </v-data-iterator>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+  //
+</script>
---
+++ fe/src/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <AppNavBar />
+
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <AppFooter />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue'
+</script>
